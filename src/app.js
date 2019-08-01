62032d63161b8b1b993b65a3110f9443f5d8cd27
Adam Brooks <adambrooks107@gmail.com>
2019-08-01 20:47:02 +1000

    first task

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ App = {
   
     load: async () => {
       await App.loadWeb3()
+      await App.loadAccount()
+      await App.loadContract()
+      await App.render()
     },
   
     // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
@@ -36,6 +39,78 @@ App = {
       // Non-dapp browsers...
       else {
         console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
+      }
+    },
+
+    loadAccount: async () => {
+      App.account = web3.eth.accounts[0]
+    },
+
+    loadContract: async () => {
+      const todoList = await $.getJSON('TodoList.json')
+      App.contracts.TodoList = TruffleContract(todoList)
+      App.contracts.TodoList.setProvider(App.web3Provider)
+
+      App.todoList = await App.contracts.TodoList.deployed()
+    },
+
+    render: async () => {
+      if (App.loading) {
+        return
+      }
+
+      App.setLoading(true)
+
+      $('#account').html(App.account)
+
+      await App.renderTasks()
+
+      App.setLoading(false)
+    },
+
+    renderTasks: async () => {
+      // Load the total task count from the blockchain
+      const taskCount = await App.todoList.taskCount()
+      const $taskTemplate = $('.taskTemplate')
+
+      // Render out each task with a new task template
+      for (var i = 1; i <= taskCount; i++) {
+        // Fetch the task data from the blockchain
+        const task = await App.todoList.tasks(i)
+        const taskId = task[0].toNumber()
+        const taskContent = task[1]
+        const taskCompleted = task[2]
+
+        // Create the html for the task
+      const $newTaskTemplate = $taskTemplate.clone()
+      $newTaskTemplate.find('.content').html(taskContent)
+      $newTaskTemplate.find('input')
+                      .prop('name', taskId)
+                      .prop('checked', taskCompleted)
+                      // .on('click', App.toggleCompleted)
+
+      // Put the task in the correct list
+      if (taskCompleted) {
+        $('#completedTaskList').append($newTaskTemplate)
+      } else {
+        $('#taskList').append($newTaskTemplate)
+      }
+
+      // Show the task
+      $newTaskTemplate.show()
+      }
+    },
+
+    setLoading: (boolean) => {
+      App.loading = boolean
+      const loader = $('#loader')
+      const content = $('#content')
+      if (boolean) {
+        loader.show()
+        content.hide()
+      } else {
+        loader.hide()
+        content.show()
       }
     }
 }
